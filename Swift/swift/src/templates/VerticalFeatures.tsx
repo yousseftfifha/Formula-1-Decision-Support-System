aafceb5fbeb7044e3a88e0977e1035b23fcf47f0
youssef tfifha <47751657+yousseftfifha@users.noreply.github.com>
2022-04-21 10:45:26 +0100

    Update

--- Swift/swift/src/templates/VerticalFeatures.tsx
+++ Swift/swift/src/templates/VerticalFeatures.tsx
@@ -3,8 +3,8 @@ const VerticalFeatures = () => (
     <div className="flex justify-center place-content-center  border-2 h-full">
       <iframe
         title="F1"
-        width="2400"
-        height="900"
+        width="2800"
+        height="1500"
         src="https://app.powerbi.com/reportEmbed?reportId=f831feb3-5179-464b-b2f0-7e1e7fcdb8c5&autoAuth=true&ctid=604f1a96-cbe8-43f8-abbf-f8eaf5d85730&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLW5vcnRoLWV1cm9wZS1pLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D"
         frameBorder="0"
         allowFullScreen
