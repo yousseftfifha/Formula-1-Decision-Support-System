a90d29639b7f5ee43b79eee5a5ea29b0d2213b55
youssef tfifha <47751657+yousseftfifha@users.noreply.github.com>
2022-05-05 11:16:42 +0100

    update

--- Swift/swift/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images:{
-    domains:['images.unsplash.com', 'via.placeholder.com', 'randomuser.me'],
-  },
-}
-
-module.exports = nextConfig
